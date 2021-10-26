d0e138b323bd20c5908a193b744bd24c5d056f23
Galaxy13 <gen.hort54@gmail.com>
2021-10-26 11:42:24 +0700

    Update: adding sorting diff, and nice tabular output

--- CVSCompare_Script.py
+++ CVSCompare_Script.py
@@ -1,12 +1,14 @@
 # mini script of comparing values in 2-column csv file
 import csv
+from prettytable import PrettyTable
 from optparse import OptionParser
 
+
 # adding and initializing option parser
-parser = OptionParser()
-parser.add_option('--f1', '--file1', dest='path1', type='string', help='gets path of first csv file')
-parser.add_option('--f2', '--file2', dest='path2', type='string', help='gets path of second csv file')
-(options, args) = parser.parse_args()
+# parser = OptionParser()
+# parser.add_option('--f1', '--file1', dest='path1', type='string', help='gets path of first csv file')
+# parser.add_option('--f2', '--file2', dest='path2', type='string', help='gets path of second csv file')
+# (options, args) = parser.parse_args()
 
 
 # defining class, appending paths of csv files and compare their 'value' column
@@ -14,7 +16,7 @@ class CsvCompare:
     def __init__(self, path1, path2):
         self.path1 = path1
         self.path2 = path2
-        self.result = []
+        self.result = {}
 
     # open files and creating a dict files
     def file(self):
@@ -27,22 +29,26 @@ class CsvCompare:
     def compare(self):
         dict1 = CsvCompare.file(self)[0]
         dict2 = CsvCompare.file(self)[1]
+        result_str = []
         for key in dict1:
             try:
                 value1 = float(dict1[key])
                 value2 = float(dict2[key])
             except:
-                print("Error: no name " + key + "in" + self.path2)
+                print("Error: no name " + key + " in " + self.path2)
                 return None
-            if dict1[key] < dict2[key]:
-                self.result.append(str(key) + ',' + dict1[key] + ',' +
-                                   dict2[key] + ', ' + str(value1 / value2 * 100))
-            elif dict1[key] > dict2[key]:
-                self.result.append(str(key) + ',' + dict1[key] + ',' + dict2[key] +
-                                   ', ' + str(value2 / value1 * 100))
-        return self.result
+            diff = round(abs((value1 - value2) / ((value1 + value2) / 2) * 100), 2)
+            self.result[key] = diff
+        self.result = {key: value for key, value in sorted(self.result.items(),
+                                                           key=lambda item: item[1])}
+        for key, value in self.result.items():
+            string = [key, dict1[key], dict2[key], str(value) + '%']
+            result_str.append(string)
+        return result_str
 
 
 # launch of method csvCompare.compare
-result = CsvCompare(options.path1, options.path2)
-print(result.compare())
+result = CsvCompare('files/1.csv', 'files/2.scv')
+t = PrettyTable(['ID', 'Value1', 'Value2', 'Diff'])
+t.add_rows(result.compare())
+print(t)
