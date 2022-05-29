f60e15cf911f70f6a08e82eab8c085f6305b0e67
Galaxy13 <gen.hort54@gmail.com>
2022-05-29 21:48:02 +0300

    Updating LeetCode "Missing Number" solution

--- leetcode_tasks/missing_num.py
+++ leetcode_tasks/missing_num.py
@@ -1,11 +1,10 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        return set(range(max(nums) + 1)) - set(nums)
+        return (set(range(len(nums) + 1)) - set(nums)).pop()
 
 
 sol = Solution()
+print (sol.missingNumber([0, 1, 2, 3, 4]))
 
-# print (sol.missingNumber([1, 2, 3, 4]))
 
-
-print(set(range(3)))
+# print(set(range(3)))
