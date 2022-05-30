e499b9b68b533c2f2655916b97d1545619c97abe
Galaxy13 <gen.hort54@gmail.com>
2022-05-30 17:23:19 +0300

    Updating simpler solution

--- leetcode_tasks/missing_num_in_seq.py
+++ leetcode_tasks/missing_num_in_seq.py
@@ -1,13 +1,16 @@
 class Solution:
+    # def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+    #     if len(nums) > len(set(nums)):
+    #         return list(set(range(1, len(nums) + 1)) - set(nums))
+    #     else:
+    #         return list(set(range(1, max(nums) + 1)) - set(nums))
+
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        if len(nums) > len(set(nums)):
-            return list(set(range(1, len(nums) + 1)) - set(nums))
-        else:
-            return list(set(range(1, max(nums) + 1)) - set(nums))
+        return list(set(range(1, len(nums) + 1)) - set(nums))
 
 
 sol = Solution()
 
-print(sol.findDisappearedNumbers([1, 1]))
+print(sol.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
 
 print(set(range(1, len([1, 2, 3]) + 1)))
