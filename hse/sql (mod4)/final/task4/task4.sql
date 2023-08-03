c91f47a982036647df02b0d0cec76c1f52240c75
Galaxy13 <gen.hort54@gmail.com>
2023-08-03 17:21:39 +0300

    Update: solving task 4

--- hse/sql (mod4)/final/task4/task4.sql
+++ hse/sql (mod4)/final/task4/task4.sql
@@ -1,27 +1,10 @@
-select distinct id,
-                S,
-                case when SRTi <= Sa then 'A'
-                when SRTi <= Sb then 'B'
-                else 'C' end,
-                orderyear from (select c2.id, extract(year from orderdate) as orderyear, sum(subtotal) as S,
-       0.8 * SUM(subtotal)  AS Sa,
-        0.95 * SUM(subtotal) as  Sb,
-        SUM(subtotal) OVER (PARTITION BY extract(year from orderdate) ORDER BY subtotal DESC) AS SRTi
-       FROM
-            salesorderheader
-        JOIN customer c ON c.customerid = salesorderheader.customerid
-        JOIN company c2 ON c.companyname = c2.cname
-where extract(year from orderdate) in (2012, 2013)
-group by c2.id, extract(year from orderdate)) as subquery;
-
-select c2.id, extract(year from orderdate) as orderyear, sum(subtotal) as S,
-       sum(subtotal) * 0.8 as Sa,
-       sum(subtotal) * 0.95 as Sb,
-       sum(sum(subtotal)) over (order by sum(subtotal) desc) as SRTi
-       FROM
-            salesorderheader
-        JOIN customer c ON c.customerid = salesorderheader.customerid
-        JOIN company c2 ON c.companyname = c2.cname
-where extract(year from orderdate) in (2012, 2013)
-group by c2.id, extract(year from orderdate)
-order by S desc
+insert into company_abc (cid, salestotal, cls, year) with STI_CTE as (
+select id, STi, year, sum(STi) over (order by STi desc) as SRTi from (select c2.id, extract(year from orderdate) as year, sum(subtotal) as STi from salesorderheader
+    JOIN customer c ON c.customerid = salesorderheader.customerid
+    JOIN company c2 ON c.companyname = c2.cname
+    WHERE EXTRACT(year FROM orderdate) IN (2012, 2013)
+group by c2.id, extract(year from orderdate)) as subquery
+group by STi, id, year)
+select id, STi, case when SRTi <= 0.8 * (select sum(STI) from STI_CTE) then 'A'
+                    when SRTI <= 0.95 * (select sum(STI) from STI_CTE) then 'B'
+                    else 'C' end as class, year from STI_CTE
